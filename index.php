e7ef1f283b6e4f8e82588b7d4ec838a69c2bc175
Xalalau Xubilozo <xalalau@outlook.com>
2022-07-31 22:49:41 -0300

    Improve error colors and add tooltips

--- index.php
+++ index.php
@@ -24,28 +24,54 @@ $html_header = <<<EOD
             background-color: #181a1b;
             color: #e8e6e3;
         }
+        #errors-table {
+        }
         #errors-th {
             background-color: #000;
         }
         td, th {
             padding: 6px;
         }
-        #title {
+        #heading {
             font-size: 2.5em;
             font-weight: bold;
         }
-        #subtitle {
+        #subheading {
             font-size: 1.5em;
             font-weight: bold;
             background-color: #5b7c00;
             padding: 5px 10px 5px 10px;
             border-radius: 8px;
         }
+        #subtitles {
+            padding: 12px 0 7px 0;
+        }
+        .tooltip {
+            position: relative;
+            display: inline-block;
+            border-bottom: 1px dotted black;
+        }
+        .tooltip .tooltiptext {
+            visibility: hidden;
+            width: 120px;
+            background-color: black;
+            color: #fff;
+            text-align: center;
+            border-radius: 6px;
+            padding: 5px;
+            position: absolute;
+            z-index: 1;
+            top: -5px;
+            left: 105%;
+        }
+        .tooltip:hover .tooltiptext {
+            visibility: visible;
+        }
     </style>
 </head>
 <body>
-<span id="title">Auto Reported Script Errors </span>
-<span id="subtitle">$addon</span></br>
+<span id="heading">Auto Reported Script Errors </span>
+<span id="subheading">$addon</span></br>
 </br>
 EOD;
 echo $html_header;
@@ -60,15 +86,26 @@ if ($errors == false) {
 
 $status = [
     [ "TO-DO", "" ],                   // 0
-    [ "TO-DO Critical", "#4c0000" ],   // 1
-    [ "Fixed", "#0c3817" ],            // 2
-    [ "Wont Fix", "#454a10" ],         // 3
+    [ "Critical", "#4c0000" ],         // 1
+    [ "Fixed", "#063a10" ],            // 2
+    [ "Wont Fix", "#4a2f10" ],         // 3
     [ "Unrelated", "#10184a" ],        // 4
     [ "Ignored", "#4f0656"]            // 5
 ];
 
+$tooltip = <<<EOD
+<div>
+<div style="background-color: #181a1b;">0 - TO-DO</div> 
+<div style="background-color: #4c0000;">1 - Critical</div> 
+<div style="background-color: #063a10;">2 - Fixed</div> 
+<div style="background-color: #4a2f10;">3 - Wont Fix</div> 
+<div style="background-color: #10184a;">4 - Unrelated</div> 
+<div style="background-color: #4f0656;">5 - Ignored</div>
+</div>
+EOD;
+
 $table_header = <<<EOD
-<table>
+<table id="errors-table">
 <tr id='errors-th'>
 <th>status</th>
 <th>info</th>
@@ -79,7 +116,12 @@ echo $table_header;
 while ($error = mysqli_fetch_array($errors)) {
     $row = <<<EOD
     <tr style='background-color: {$status[$error['status']][1]};'>
-        <td>{$status[$error['status']][0]}</td>
+        <td>
+            <div class="tooltip">
+                {$status[$error['status']][0]}
+                <span class="tooltiptext">$tooltip</span>
+            </div>
+        </td>
         <td>{$error['datetime']}</br>{$error['map']}</br>{$error['quantity']} times</td>
         <td>
             <pre>{$error['message']}</br>{$error['stack']}</pre>
